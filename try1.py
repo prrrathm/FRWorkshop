adddd1ce5eb398bd17796a25807911a0f7177b26
Pratham Sharma <prrrathm@gmail.com>
2021-10-30 14:32:07 +0530

    everything has changed

--- try1.py
+++ try1.py
@@ -1,4 +1,7 @@
 import face_recognition
-
-l = face_recognition.load_image_file('img/images')
-print(l)
+from PIL import Image, ImageDraw, ImageFont
+pil_sunset = Image.open('img/images(3)')
+draw = ImageDraw.Draw(pil_sunset)
+draw.text((500, 350), "Sunset Picture", fill=(0, 0, 255))
+pil_sunset.show()
+Image.open('img/images4').show()
